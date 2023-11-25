55896df40774654b075b747fae14476aacf280e9
Paul Bauer <waaa.up@gmail.com>
2023-11-25 10:18:09 -0600

    Initial commit

--- CSC450_PortfolioProject/main.cpp
+++ CSC450_PortfolioProject/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mute;
+
+void count(int start, int end, int increment){
+    std::lock_guard<std::mutex>lock(mute); //lock so that it is not interrupted
+    
+    for(int i = start; i != (end + increment); i += increment){
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::thread t1(count, 0, 20, 1);
+    std::thread t2(count, 20, 0, -1);
+    
+    t1.join();
+    t2.join();
+    
     return 0;
 }
